c1aa58b6964f66946ce4754406aa272a2b62aefa
PAGARAN <2201101859@student.buksu.edu.ph>
2024-10-20 20:09:13 +0800

    Nag Add ko og Add Student tapos Get by Name nga routes

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ app.use(express.json());
 
 
 // API Routes
-// Get all documents
-app.get('/api/documents', (req, res) => {
+
+// Add Student
+app.post('/api/documents', (req, res) => {
    
 });
 
+// Get all documents
+app.get('/api/documents/all', (req, res) => {
+   
+});
+
+// Get by ID
 app.get('/api/documents/i/:id', (req, res) => {
+    
+});
+
+// Get by Name
+app.get('/api/documents/n/:name', (req, res) => {
     
 });
