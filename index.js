cc6c5a66c85e70eeb2f608c571f0e28bc88a59fc
Vincent2366 <2001105940@student.buksu.edu.ph>
2024-10-20 19:38:05 +0800

    index.js is added together with 2 API features, (get all documents and get documents by ID)

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+
+// API Routes
+// Get all documents
+app.get('/api/documents', (req, res) => {
+   
+});
+
+app.get('/api/documents/i/:id', (req, res) => {
+    
+});
