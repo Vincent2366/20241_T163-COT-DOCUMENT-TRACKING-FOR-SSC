1333397015392d7e69fdb799f2aa9e6c00f0df39
Vincent2366 <2001105940@student.buksu.edu.ph>
2024-10-28 18:35:34 +0800

    added more routes

--- index.js
+++ index.js
@@ -30,3 +30,23 @@ app.put('/api/documents/:id', async (req, res) => {
 app.delete('/api/documents/:id', async (req, res) => {
 
 });
+
+// API Routes
+// Get all documents
+app.get('/api/transferIn/all', (req, res) => {
+   
+});
+
+// Get document by ID
+app.get('/api/transferIn/i/:id', (req, res) => {
+
+});
+
+app.get('/api/transferIn//:name', (req, res) => {
+
+});
+
+// Create a new document
+app.post('/api/transferIn', async (req, res) => {
+
+});
