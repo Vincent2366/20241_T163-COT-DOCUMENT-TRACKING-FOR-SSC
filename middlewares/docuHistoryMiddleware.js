50e9e83d3c8a71baae561991158de33b8656f422
PAGARAN <2201101859@student.buksu.edu.ph>
2024-10-29 12:36:53 +0800

    Added docuHistoryMiddleware and docuHistoryRoute

---
+++ middlewares/docuHistoryMiddleware.js
@@ -0,0 +1,9 @@
+const validateDocumentId = (req, res, next) => {
+    const { id } = req.params;
+    if (!id || isNaN(id)) { // Adjust this condition as needed based on your ID validation logic
+        return res.status(400).json({ message: 'Invalid document ID' });
+    }
+    next();
+};
+
+module.exports = validateDocumentId;
