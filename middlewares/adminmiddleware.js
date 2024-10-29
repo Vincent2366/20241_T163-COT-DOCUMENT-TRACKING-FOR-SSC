5f5a4fd28312313519854ad6ef57c4c2f7d4d607
Vincent2366 <2001105940@student.buksu.edu.ph>
2024-10-30 06:28:44 +0800

    delete

--- middlewares/adminmiddleware.js
+++
@@ -1,9 +0,0 @@
-
-exports.isAdmin = (req, res, next) => {
-    const userRole = req.user.role; // Assuming req.user is set after authentication
-
-    if (userRole !== 'Admin') {
-        return res.status(403).json({ message: 'Access denied: Admins only.' });
-    }
-    next();
-};
