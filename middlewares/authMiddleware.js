fcae5e62262c0fd0df25d5ae4a9029c5a2180822
Vincent2366 <2001105940@student.buksu.edu.ph>
2024-10-29 10:09:46 +0800

    restructured

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = process.env;
+
+exports.verifyToken = (req, res, next) => {
+  const token = req.header('Authorization')?.split(' ')[1];
+  if (!token) return res.status(401).json({ error: 'Access denied, no token provided' });
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(400).json({ error: 'Invalid token' });
+  }
+};
